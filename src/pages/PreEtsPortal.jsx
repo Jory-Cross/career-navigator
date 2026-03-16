a13854994456969eb3ab20493f39898c4fd9e849
jory Cross <arbranger@gmail.com>
2026-03-16 15:23:02 +0000

    File changes

--- src/pages/PreEtsPortal.jsx
+++ src/pages/PreEtsPortal.jsx
@@ -14,6 +14,7 @@ import { cn } from "@/lib/utils";
 import { toast } from "sonner";
 import IEPPlanSection from "@/components/pre-ets/IEPPlanSection";
 import SkillsExplorationTab from "@/components/pre-ets/SkillsExplorationTab";
+import ClockInOut from "@/components/pre-ets/ClockInOut";
 
 const CHECKLIST_ITEMS = [
   { key: "career_exploration", label: "Complete Career Exploration Assessment", category: "assessments" },
