ec0fa709717309720ff05c6253d649dfe8cc1c14
jory Cross <arbranger@gmail.com>
2026-03-20 01:03:31 +0000

    File changes

--- src/components/client-portal/AgentChatEmbed.jsx
+++ src/components/client-portal/AgentChatEmbed.jsx
@@ -84,7 +84,7 @@ export default function AgentChatEmbed({ agentKey, title, description, clientId 
   };
 
   const clearConversation = async () => {
-    const storageKey = `agent_conv_${agentKey}`;
+    const storageKey = `agent_conv_${agentKey}_${clientId || 'anonymous'}`;
     localStorage.removeItem(storageKey);
     setMessages([]);
     setConversation(null);
