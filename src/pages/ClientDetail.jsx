8543f618ab4d8f96efca94ef648848af00efcea6
Jory-Cross <arbranger@gmail.com>
2026-04-19 02:47:23 -0600

    Update ClientDetail.jsx

--- src/pages/ClientDetail.jsx
+++ src/pages/ClientDetail.jsx
@@ -487,9 +487,8 @@ const handleDocumentsChanged = useCallback(() => {
        <>
   {!isClientUser && !isEmployed && (
    <TabsContent value="client_details">
- <div className="space-y-6">
-  <div className="bg-white rounded-2xl border p-4">
-    <ClientHeader client={client} onUpdate={refreshClient} formOnly />
+<div className="space-y-6">
+  <ClientHeader client={client} onUpdate={refreshClient} formOnly />
   </div>
 
     {!isDspd && !isEmployed && !isClientUser && (
