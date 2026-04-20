5f8a521362d8ad784eed37bc1e5aabddc7805ad1
Jory-Cross <arbranger@gmail.com>
2026-04-19 18:53:54 -0600

    Update DocumentsSection.jsx

--- src/components/client-detail/DocumentsSection.jsx
+++ src/components/client-detail/DocumentsSection.jsx
@@ -334,15 +334,7 @@ const suggestedJobs = resumeSkills.length > 0
     </Button>
   )}
 </div>
-            {resumeSkills.length > 0 && (
-  <div className="flex flex-wrap gap-1 mt-2 mb-3">
-    {resumeSkills.map((skill) => (
-      <Badge key={skill} className="text-xs bg-blue-50 text-blue-700 border border-blue-200">
-        {skill}
-      </Badge>
-    ))}
-  </div>
-)}
+            
           </div>
 
           <div className="flex gap-2 flex-wrap">
