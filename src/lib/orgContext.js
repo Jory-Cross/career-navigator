7b6cb40c758bba5dcea3f167ead3bc79802aec17
Jory-Cross <arbranger@gmail.com>
2026-04-20 23:31:26 -0600

    Update orgContext.js

--- src/lib/orgContext.js
+++ src/lib/orgContext.js
@@ -1,1 +1,13 @@
+let currentOrgId = null;
 
+export function setOrgId(orgId) {
+  currentOrgId = orgId || null;
+}
+
+export function getOrgId() {
+  return currentOrgId;
+}
+
+export function clearOrgId() {
+  currentOrgId = null;
+}
