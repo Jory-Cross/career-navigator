227c65a5a207f8fd631b0e299de7c67244f943ab
Jory-Cross <arbranger@gmail.com>
2026-04-24 09:01:00 -0600

    Update generateRecommendationBatch.js

--- src/lib/recommendations/generateRecommendationBatch.js
+++ src/lib/recommendations/generateRecommendationBatch.js
@@ -1,1 +1,21 @@
+import { getRecommendations } from "@/lib/adapters/recommendationAdapter";
 
+export async function generateRecommendationBatch({
+  client,
+  resumes = [],
+  wsa = null,
+  otherAssessments = [],
+  interestProfile = null,
+  options = {}
+} = {}) {
+  return getRecommendations({
+    client,
+    resumes,
+    wsa,
+    otherAssessments,
+    interestProfile,
+    options
+  });
+}
+
+export default generateRecommendationBatch;
