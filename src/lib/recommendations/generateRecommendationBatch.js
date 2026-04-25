3353aea18d4c425265cc5fa1d2a0c8521870badd
Jory-Cross <arbranger@gmail.com>
2026-04-25 12:56:36 -0600

    Update generateRecommendationBatch.js

--- src/lib/recommendations/generateRecommendationBatch.js
+++ src/lib/recommendations/generateRecommendationBatch.js
@@ -1,3 +1,4 @@
+import { base44 } from "@/api/base44Client";
 import { getOnetRecommendations } from "@/lib/recommendations/getOnetRecommendations";
 
 export async function generateRecommendationBatch({
