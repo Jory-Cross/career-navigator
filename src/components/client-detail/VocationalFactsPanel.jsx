81da780c208fd0e4a485137cb459715e31b48c7e
Jory-Cross <arbranger@gmail.com>
2026-04-26 21:30:49 -0600

    Update VocationalFactsPanel.jsx

--- src/components/client-detail/VocationalFactsPanel.jsx
+++ src/components/client-detail/VocationalFactsPanel.jsx
@@ -158,14 +158,16 @@ export default function VocationalFactsPanel({ clientId, client, onFactsUpdated 
     "";
 
   const docCount =
-    client?.vocational_facts_document_count ??
-    metadata.source_document_ids?.length ??
-    0;
+  client?.vocational_facts_document_count ??
+  metadata.source_document_ids?.length ??
+  client?.vocational_facts_profile?.documents_processed ??
+  0;
 
-  const assessCount =
-    client?.vocational_facts_assessment_count ??
-    metadata.source_assessment_ids?.length ??
-    0;
+const assessCount =
+  client?.vocational_facts_assessment_count ??
+  metadata.source_assessment_ids?.length ??
+  client?.vocational_facts_profile?.assessments_processed ??
+  0;
 
     const handleExtract = async () => {
     setExtracting(true);
