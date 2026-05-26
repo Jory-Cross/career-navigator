8d045be2155b160dc7d4b68489d30c7b02595092
Jory-Cross <arbranger@gmail.com>
2026-05-25 21:23:31 -0600

    Update ClientDetail.jsx

--- src/pages/ClientDetail.jsx
+++ src/pages/ClientDetail.jsx
@@ -31,6 +31,7 @@ import {
   getTasks,
   getArchivedTasks,
   getTimeEntries,
+  getOnboardingSteps,
 } from "@/lib/api/clientPortalApi";
 
 import ClientHeader from "@/components/client-detail/ClientHeader";
