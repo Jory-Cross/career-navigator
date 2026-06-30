231c9970251ea9c108909a998c9301b7004496ec
Jory-Cross <arbranger@gmail.com>
2026-06-29 19:20:31 -0600

    Update toaster.jsx

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -1,3 +1,4 @@
+import { Toaster as SonnerToaster } from "sonner";
 import { useToast } from "@/components/ui/use-toast";
 import {
   Toast,
@@ -12,22 +13,30 @@ export function Toaster() {
   const { toasts } = useToast();
 
   return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        );
-      })}
-      <ToastViewport />
-    </ToastProvider>
+    <>
+      <ToastProvider>
+        {toasts.map(function ({ id, title, description, action, ...props }) {
+          return (
+            <Toast key={id} {...props}>
+              <div className="grid gap-1">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
+              {action}
+              <ToastClose />
+            </Toast>
+          );
+        })}
+        <ToastViewport />
+      </ToastProvider>
+
+      <SonnerToaster
+        position="top-right"
+        richColors
+        closeButton
+      />
+    </>
   );
-} 
+}
