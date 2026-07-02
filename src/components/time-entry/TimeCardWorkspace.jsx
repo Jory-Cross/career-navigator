2d8a8d9e613dd346c3080c958387af7d539330ad
Jory-Cross <arbranger@gmail.com>
2026-07-02 00:21:00 -0600

    Update TimeCardWorkspace.jsx

--- src/components/time-entry/TimeCardWorkspace.jsx
+++ src/components/time-entry/TimeCardWorkspace.jsx
@@ -1273,7 +1273,8 @@ export default function TimeCardWorkspace({
             ))}
           </div>
         )}
-      </Card>
+             </Card>
+      ) : null}
 
       {selectedCard ? (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 p-4">
