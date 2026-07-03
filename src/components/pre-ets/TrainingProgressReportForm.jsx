144e94943c7911115d87e20b4cc7457bfe092b19
Jory-Cross <arbranger@gmail.com>
2026-07-03 00:51:27 -0600

    Update TrainingProgressReportForm.jsx

--- src/components/pre-ets/TrainingProgressReportForm.jsx
+++ src/components/pre-ets/TrainingProgressReportForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
-import { useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
